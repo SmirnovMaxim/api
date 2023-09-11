0cdf3db9ad31fc676b11303e652b554fe2ecd908
Maxim Smirnov <watercolord@gmail.com>
2023-09-12 00:31:41 +0300

    feat: Update client

--- app/src/client/dto/updateClientDto.ts
+++ app/src/client/dto/updateClientDto.ts
@@ -5,27 +5,27 @@ import {IsUnique} from '../../validators';
 export class UpdateClientDto {
   @ApiProperty({ example: 'Иван', description: 'Имя', required: false })
   @IsString()
-  @ValidateIf((dto) => dto.firstName)
+  @ValidateIf((dto) => Object.prototype.hasOwnProperty.call(dto, 'firstName'))
   readonly firstName: string;
 
   @ApiProperty({ example: 'Иванов', description: 'Фамилия', required: false })
   @IsString()
-  @ValidateIf((dto) => dto.lastName)
+  @ValidateIf((dto) => Object.prototype.hasOwnProperty.call(dto, 'lastName'))
   readonly lastName: string;
 
   @ApiProperty({ example: 'Иванов', description: 'Отчество', required: false })
   @IsString()
-  @ValidateIf((dto) => dto.surName)
+  @ValidateIf((dto) => Object.prototype.hasOwnProperty.call(dto, 'surName'))
   readonly surName: string;
 
   @ApiProperty({ example: 'ivanov@mail.ru', description: 'E-mail', required: false })
   @IsEmail()
   @IsUnique({ column: 'email', tableName: 'client' })
-  @ValidateIf((dto) => dto.email)
+  @ValidateIf((dto) => Object.prototype.hasOwnProperty.call(dto, 'email'))
   readonly email: string;
 
   @ApiProperty({ example: 'id123', description: 'ID VK', required: false })
   @IsString()
-  @ValidateIf((dto) => dto.vkId)
+  @ValidateIf((dto) => Object.prototype.hasOwnProperty.call(dto, 'vkId'))
   readonly vkId?: string;
 }
